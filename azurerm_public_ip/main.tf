5c290b0b45a89f1b562cdd590192ccbd27bf17c4
arya1228 <priyankasrivastava89@gmail.com>
2025-12-20 00:54:06 +0530

    Code commit

--- azurerm_public_ip/main.tf
+++ azurerm_public_ip/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "pip" {
-  name = var.pip_name
+  name                = var.pip_name
   resource_group_name = var.pip_rgname
-  location = var.pip_location
-  allocation_method = var.pip_allmet
-  sku = "Basic"
+  location            = var.pip_location
+  allocation_method   = var.pip_allmet
+  sku                 = "Standard"
 }
